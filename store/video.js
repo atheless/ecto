25fc38665cf168758a322e9b23bdd6eb39a02806
atheless <gfrozontle@gmail.com>
2024-04-29 23:04:09 +0200

    init

--- store/video.js
+++ store/video.js
@@ -4,26 +4,19 @@ export const useVideoStore = defineStore("video", {
   state: () => ({
     videos: [],
     db: null,
-    // other states
   }),
   actions: {
     async initializeDB() {
-      // IndexedDB initialization logic
     },
     startRecording() {
-      // Start recording logic
     },
     loadLastVideo() {
-      // Load last video logic
     },
     playVideo(index) {
-      // Play video logic
     },
     downloadVideo(index) {
-      // Download video logic
     },
     deleteVideo(index) {
-      // Delete video logic
     },
   },
 });
